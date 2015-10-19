5e1ad14518941d23cc284bd05f9d86816e458327
Benjamin J Anderson <banderson@mashable.com>
2015-10-18 22:38:26 -0500

    Message form does not need to be a flux component

--- src/scripts/components/message_form.jsx
+++ src/scripts/components/message_form.jsx
@@ -3,25 +3,40 @@ import Dispatcher     from '../dispatcher/default';
 import React          from 'react';
 import Styles         from '../../styles/global';
 
-export default class MessageForm extends React.Component {
+export default React.createClass({
+  
+  getInitialState(){
+    return this.state = {
+      text: ''
+    };
+  },
+  
+  handleChange(event){
+    this.setState({
+      text: event.target.value
+    });
+  },
   
   send_message() {
     let message = this.refs.message.getDOMNode();
     if(message.value != ''){
       Dispatcher.dispatch(ACTIONS.MSG.SEND, message.value);
-      message.value = '';
+      this.setState(
+        this.getInitialState()
+      );
     }
-  }
+  },
   
   render() {
+    console.log(this.state);
     return (
       <div>
         <form className='message-form' onSubmit={this.send_message.bind(this)}>
-          <input type="text" className='form-control message' placeholder="Send A Message...." ref='message' />
+          <input type="text" className='form-control message' placeholder="Send A Message...." ref='message' value={this.state.text} onChange={this.handleChange} />
           <button type="submit" id="login-button" className="btn btn-primary send">Send</button>
         </form>
       </div>
     );
   }
    
-}
+});
